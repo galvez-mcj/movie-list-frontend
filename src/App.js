e90711cd3f8bca3a3d608970bdc3ce736ac1a22b
tinay <galvez.mcj@gmail.com>
2023-04-20 11:06:55 +0800

    added components and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import logo from './logo.png';
 import './App.css';
+import api from './api/axiosConfig';
+import { useState, useEffect } from 'react';
+import Layout from './components/Layout';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
+  const [movies, setMovies] = useState();
+  
+  const getMovies = async () => {
+
+    try {
+      const response = await api.get("/api/v1/movies");
+      console.log(response.data);
+      setMovies(response.data);
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <div className="App">
-      Movie App
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home movies={movies}/>} />
+        
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 // to overcome CORS block
+// export default axios.create({
+//     baseURL: 'https://9c96-103-106-239-104.ap.ngrok.io',
+//     headers: {"ngrok-skip-browser-warning": "true"}
+// });
+
 export default axios.create({
-    baseURL: 'https://9c96-103-106-239-104.ap.ngrok.io',
-    headers: {"ngrok-skip-browser-warning": "true"}
+    baseURL: 'http://localhost:8080',
+    //headers: {"ngrok-skip-browser-warning": "true"}
 });
